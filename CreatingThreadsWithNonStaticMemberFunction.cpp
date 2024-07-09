c846479dfc2e4ed1f66924777e3644ff2d2c1e11
ipekakpinar <115181387+ipekakpinar@users.noreply.github.com>
2024-07-09 23:48:39 +0300

    Create CreatingThreadsWithNonStaticMemberFunction.cpp

---
+++ CreatingThreadsWithNonStaticMemberFunction.cpp
@@ -0,0 +1,27 @@
+// 5 FARKLI ŞEKİLDE  THREAD OLUŞTURABİLİRİZ.
+//
+// NOT: EĞER BİRDEN FAZLA THREADİ AYNI ANDA OLUŞTURURSAK, HANGİ THREADİN İLK BAŞLAYACAĞININ GARANTİSİ YOKTUR
+//
+
+#include <iostream>
+#include <thread>
+using namespace std;
+
+
+////NON STATIC MEMBER FUNCTION
+
+class Base {
+public:
+	void run(int x) {
+		while (x-- > 0) {
+			cout << x << endl;
+		}
+	}
+};
+
+int main() {
+	Base b;
+	std::thread t1(&Base::run,&b,10);
+	t1.join();
+	return 0;
+}
