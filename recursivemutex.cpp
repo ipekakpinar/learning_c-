dd47421b43a4a5b033818bc65d4ce49c4e7c3bb1
ipekakpinar <115181387+ipekakpinar@users.noreply.github.com>
2024-08-03 23:40:00 +0300

    Create recursivemutex.cpp

---
+++ recursivemutex.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <thread>
+#include <mutex>
+#include <chrono>
+using namespace std;
+
+std::recursive_mutex m1;
+int buffer = 0;
+
+void recursion(char c, int loopFor) {
+
+	if (loopFor < 0) {
+		return;
+	}
+	m1.lock();
+	cout << c << " " << buffer++ << endl;
+	recursion(c, --loopFor);
+	m1.unlock();
+}
+int main() {
+	thread t1(recursion, '0', 10);
+	thread t2(recursion, '1', 10);
+	t1.join();
+	t2.join();
+	return 0;
+}
